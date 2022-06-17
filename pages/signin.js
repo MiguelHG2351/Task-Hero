16dde0a379f7ccdd064e3f38eda9c0f565194ee3
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-06-17 17:07:43 +0000

    auth with google and facebook

--- pages/signin.js
+++ pages/signin.js
@@ -1,1 +1,23 @@
-import { providers, si } from 'next-auth/client'
+import { getProviders, signIn, getSession, getCsrfToken } from 'next-auth/react'
+
+export default function signin({ providers }) {
+  return (
+    <div>
+        { Object.values(providers).map(provider => {
+            return (
+                <div onClick={() => signIn(provider.id)} key={provider.name}>
+                    <button>Sign in with {provider.name}</button>
+                </div>
+            )
+        }) }
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+    return {
+        props: {
+            providers: await getProviders(context)
+        }
+    }
+}
