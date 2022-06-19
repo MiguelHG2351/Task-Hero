cc0233a40e385a5ed00ece31f79a1f9dfe65c8a2
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-06-19 00:18:15 -0600

    new status added

---
+++ .pscale/cli-helper-scripts/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+raw_output=`pscale deploy-request list "$DB_NAME" --org "$ORG_NAME" --format json`
+
+output=`echo $raw_output | jq "[.[] | select(.state == \"open\") ] | .[0].deployment.deployable "`
+
+echo $output
+
+if [[ 1 -ne 0 ]]; then
+    echo "No open deployment request found: $raw_output"
+    exit 5
+fi
+
+if [[ $output = true ]]; then
+    echo "$DB_NAME" "$ORG_NAME" "$output"
+else
+    echo "No open deployment request found: $raw_output"
+    exit 3
+fi
+
