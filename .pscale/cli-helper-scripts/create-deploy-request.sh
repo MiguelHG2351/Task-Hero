41a916c7ea5bb6101b1ac4b5668d7f45ad63f5aa
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-06-22 08:20:50 -0600

    change deploy request action

--- .pscale/cli-helper-scripts/create-deploy-request.sh
+++ .pscale/cli-helper-scripts/create-deploy-request.sh
@@ -6,20 +6,21 @@
 . ps-create-helper-functions.sh
 
 # last deploy request
-raw_output=`pscale deploy-request list "$DB_NAME" --org "$ORG_NAME" --format json`
-if [ $? -ne 0 ]; then
-    echo "Error: pscale deploy-branch list returned non-zero exit code $?: $raw_output"
-    exit 1
-fi
+# raw_output=`pscale deploy-request list "$DB_NAME" --org "$ORG_NAME" --format json`
+# if [ $? -ne 0 ]; then
+#     echo "Error: pscale deploy-branch list returned non-zero exit code $?: $raw_output"
+#     exit 1
+# fi
 
-output=`echo $raw_output | jq ".[0].number "`
+# output=`echo $raw_output | jq ".[0].number "`
 
+create-deploy-request "${DB_NAME}" "${BRANCH_NAME}" "${ORG_NAME}"
 # Diff changes
-deploy_request_number=$output
-create-diff-for-ci "$DB_NAME" "$ORG_NAME" "$deploy_request_number" "$BRANCH_NAME" "$refresh_schema"
-STRLENGTH=`echo -n $? | wc -m`
-if [ $STRLENGTH -ne 0 ]; then
-    echo "No hay diferencias en la base de datos"
-else
-    create-deploy-request "${DB_NAME}" "${BRANCH_NAME}" "${ORG_NAME}"
-fi
+# deploy_request_number=$output
+# create-diff-for-ci "$DB_NAME" "$ORG_NAME" "$deploy_request_number" "$BRANCH_NAME" "$refresh_schema"
+# STRLENGTH=`echo -n $? | wc -m`
+# if [ $STRLENGTH -ne 0 ]; then
+#     echo "No hay diferencias en la base de datos"
+# else
+#     echo "Hay diferencias en la base de datos"
+# fi
