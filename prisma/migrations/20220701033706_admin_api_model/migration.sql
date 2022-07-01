f77236d422b357cb2d29cd07f6b230564c50f72c
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-07-01 03:38:19 +0000

    cambios en los modelos

--- prisma/migrations/20220701033706_admin_api_model/migration.sql
+++ prisma/migrations/20220701033706_admin_api_model/migration.sql
@@ -162,3 +162,12 @@ CREATE TABLE `VerificationToken` (
     UNIQUE INDEX `VerificationToken_token_key`(`token`),
     UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AdminAPI` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
